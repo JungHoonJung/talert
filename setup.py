9ea32edf8ab6680de82f60484a027cfc173c6c68
junghoon.jung <jh.jung1380@gmail.com>
2020-07-22 18:30:16 +0900

    v0.1

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='talert',
+		version='0.1',
+		description='Telegram bot for workalert',
+		author='Junghoon Jung',
+		author_email='jh.jung@uos.ac.kr',
+		packages=find_packages(),
+		setup_requires=['python-telegram-bot'],
+	 )
---
+++ talert/auth/__init__.py
@@ -0,0 +1,28 @@
+import os
+import pkg_resources
+import pickle
+
+
+def auth_user():
+    '''user identifying.'''
+    global users
+    #refer  configure of home
+    home = os.path.expanduser('~')
+    if os.path.isfile(os.path.join(home, '.talert','config')):
+        with open(os.path.join(home, '.talert','config')) as f:
+            name = f.readline().rstrip()
+    else:
+        name = input("Please let me know your chat_id : ")
+        os.makedirs(os.path.join(home, '.talert'), exist_ok = True)  # make configure folder
+        with open(os.path.join(home, '.talert','config'),'w') as f:   # save user name
+            f.write(name)
+
+
+
+    return name, users[name]
+
+def set_id(chat_id):
+    name = input("Please let me know your chat_id : ")
+    os.makedirs(os.path.join(home, '.talert'), exist_ok = True)  # make configure folder
+    with open(os.path.join(home, '.talert','config'),'w') as f:   # save user name
+        f.write(name)
